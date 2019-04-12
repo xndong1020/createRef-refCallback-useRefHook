2ab0067024e5c4a74218f9008a8836b3c622824d
isdance <isdance2004@hotmail.com>
2019-04-13 00:31:37 +1000

    use React.forwardRef

--- src/App.js
+++ src/App.js
@@ -7,35 +7,19 @@ class App extends Component {
     super(props)
     // create a ref to store the textInput DOM element
     this.textInput = React.createRef()
-
-    this.state = {
-      text: ''
-    }
-
-    this.handleInputChange = this.handleInputChange.bind(this)
   }
 
-  handleInputChange(e) {
-    // ref is used on an HTML element, the ref created in the constructor with React.createRef() receives the underlying DOM element as its current property.
-    // this.setState({ text: this.textInput.current.value })
-
-    // 由于ref 现在指向一个custom component, 所以目前this.textInput.current 的value 是这个MyInput component
-    console.log('handleInputChange', this.textInput.current, e.target.value)
+  handleSubmit = () => {
+    console.log(this.textInput.current.value)
   }
 
   render() {
-    const { text } = this.state
     // tell React that we want to associate the <input> ref
     // with the `textInput` that we created in the constructor
     return (
       <div>
-        {/* <input
-          type="text"
-          ref={this.textInput}
-          onChange={this.handleInputChange}
-        /> */}
-        <MyInput ref={this.textInput} handleChange={this.handleInputChange} />
-        <h3>{text}</h3>
+        <MyInput ref={this.textInput} />
+        <button onClick={this.handleSubmit}>Submit</button>
       </div>
     )
   }
--- src/MyInput.jsx
+++ src/MyInput.jsx
@@ -1,15 +1,9 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-export default class MyInput extends Component {
-  handleInputChange = e => {
-    const { handleChange } = this.props
-    handleChange(e)
-  }
-  render() {
-    return (
-      <>
-        <input type="text" onChange={this.handleInputChange} />
-      </>
-    )
-  }
+// ref 必须是第二个参数
+const MyInput = (props, ref) => {
+  return <input type="text" ref={ref} {...props} />
 }
+
+export default React.forwardRef(MyInput)
+
